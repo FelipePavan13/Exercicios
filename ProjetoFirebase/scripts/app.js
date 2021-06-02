2c8bf0654ee544bb3b2928e8d2967afa5993d34d
Felipe Pavan <felipepavang@hotmail.com>
2021-06-01 22:03:45 -0300

    create db

---
+++ ProjetoFirebase/scripts/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1WSUFOys_2XEvSEncseuMiIdmc2TT_Oo",
+    authDomain: "teste-firebase-6e591.firebaseapp.com",
+    projectId: "teste-firebase-6e591",
+    storageBucket: "teste-firebase-6e591.appspot.com",
+    messagingSenderId: "830856975516",
+    appId: "1:830856975516:web:cef2b55e7449f2fca30959",
+    measurementId: "G-V3Y7C2YMHX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
