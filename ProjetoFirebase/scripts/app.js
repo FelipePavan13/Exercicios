110d18a1397ceee01cfb89abcb1c4fdc19017f6c
Felipe Pavan <felipepavang@hotmail.com>
2021-06-02 00:52:33 -0300

    finish firestore exercise

--- ProjetoFirebase/scripts/app.js
+++ ProjetoFirebase/scripts/app.js
@@ -13,32 +13,62 @@
   firebase.initializeApp(firebaseConfig);
 
 //Criação da variável pra acessar o banco de dados
-  let db = firebase.firestore();
+let db = firebase.firestore();
 
-  let auth = firebase.auth();
+function ler() {
+    
+    db.collection('info').get()
+    .then((snapshot)=>{
+        snapshot.forEach((doc)=>{
+            console.log(doc.data());
+        })
+    }).catch((err)=>{
+        console.log('Permissão insuficiente. Favor realizar login.');
+    })
+}
 
-// function createUser() {
-//     let newUserEmail = "novoteste@teste.com";
-//     let newUserPassword = "123abc";
+function escrever() {
+   
+    db.collection('info')
+    .add({ title: 'Novo Objeto', number: Math.random() })
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
 
-//     auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
-//     .then((user)=>{
-//         console.log(user);
-//     }).catch((error)=>{
-//         console.log(error);
-//     })
-// }
+let auth = firebase.auth();
+
+function createUser() {
+    let newUserEmail = "novoteste@teste.com";
+    let newUserPassword = "123abc";
+
+    auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
+    .then((user)=>{
+        console.log(user);
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
 
 function login() {
     let userEmail = "novoteste@teste.com";
     let userPassword = "123abc";
 
-    auth.signInWithEmailAndPassword(userEmail, userPassword)
+    //Essa linha serve pra setar a persistência do login. Temos 3 opções: LOCAL (persiste em todo o browser, independente se abrir o site em outra aba, janela, etc), SESSION (só funciona naquela aba ou janela, se abrir outra, estará deslogado) e NONE (atualizando a página, mesmo que seja na mesma janela, desloga).
+    auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(()=>{
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
         .then((loggedUser) => {
             console.log(auth.currentUser);
         }).catch((error) => {
             console.log(error);
         });
+    }).catch((error) => {
+        console.log(error);
+    })
 }
 
 // Método pra saber se existe ou não um usuário logado
@@ -60,4 +90,4 @@ function logout() {
     })
 }
 
-// setTimeout(logout, 2000)
+// setTimeout(login, 2000);
